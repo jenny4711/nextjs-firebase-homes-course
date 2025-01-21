6ce9691749bff040016a16c1755995fc45534fbc
jiylee <jjenny4711@gmail.com>
2025-01-21 04:02:34 -0500

    saved3

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,50 +1,54 @@
 import { auth } from '@/firebase/server';
 import { DecodedIdToken } from 'firebase-admin/auth';
-import { cookies } from 'next/headers'
+import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
-
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
 import UpdatePasswordForm from './update-password-form';
 import DeleteAccountButton from './delete-account-button';
-export default async function Account(){
+export default async function Account() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get('firebaseAuthToken')?.value;
+  if (!token) {
+    redirect('/');
+  }
+  let decodedToken: DecodedIdToken;
+  try {
+    decodedToken = await auth.verifyIdToken(token);
+  } catch (error) {
+    redirect('/');
+  }
 
-const cookieStore = await cookies();
-const token = cookieStore.get("firebaseAuthToken")?.value;
-if(!token){
-  redirect("/")
-}
-let decodedToken:DecodedIdToken;
-try{
-  decodedToken = await auth.verifyIdToken(token);
-}catch(error){
- redirect("/")
-}
+  const user = await auth.getUser(decodedToken.uid);
 
-const user =await auth.getUser(decodedToken.uid);
+  const isPasswordProvider = user?.providerData.find(
+    (provider) => provider.providerId === 'password'
+  );
 
-const isPasswordProvider = user?.providerData.find(provider=>provider.providerId ==="password")
-
-  return(
+  return (
     <div className="max-w-screen-sm mx-auto">
       <Card className="mt-10">
         <CardHeader>
-          <CardTitle className="text-3xl font-bold">
-My Account
-          </CardTitle>
+          <CardTitle className="text-3xl font-bold">My Account</CardTitle>
         </CardHeader>
         <CardContent>
           <Label>Email</Label>
           <div>{decodedToken.email}</div>
-          {!!isPasswordProvider && <UpdatePasswordForm/>}
+          {!!isPasswordProvider && <UpdatePasswordForm />}
         </CardContent>
-        {!decodedToken.admin &&
-        <CardFooter className='flex flex-col items-start'>
-          <h2 className='text-red-500 text-2xl font-bold mb-2'>Danger Zon</h2>
-          <DeleteAccountButton/>
-        </CardFooter>
-        }
+        {!decodedToken.admin && (
+          <CardFooter className="flex flex-col items-start">
+            <h2 className="text-red-500 text-2xl font-bold mb-2">Danger Zon</h2>
+            <DeleteAccountButton />
+          </CardFooter>
+        )}
       </Card>
     </div>
-  )
+  );
 }
