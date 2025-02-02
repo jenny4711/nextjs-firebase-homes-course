cd16883197cf1d7844ac3a3a6c83037592114d62
jiylee <jjenny4711@gmail.com>
2025-02-01 22:06:38 -0500

    udated02012025

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,40 @@
 import Image from "next/image";
-
-export default function Home() {
+import { Carousel ,CarouselContent,CarouselItem, CarouselNext, CarouselPrevious} from '@/components/ui/carousel';
+import { getPropertyById } from '@/data/properties';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+export default async function Home() {
+  const property = await getPropertyById('a19tVPQB2kh7rd4Enb83')
+  console.log(property,'property')
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-sans]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-mono]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="min-h-screen w-full bg-[url('/home.jpg')] bg-cover bg-center bg-fixed  ">
+        
+      <main className="relative flex flex-col gap-8 items-center sm:items-start min-h-screen">
+      <div className="absolute inset-0 z-10 bg-gradient-to-l from-transparent via-black/30 to-black/70 flex-col">
+
+     <div className="w-[500px] flex item-center justify-center mt-24">
+  <h1 className="text-6xl font-bold text-white w-[400px] leading-[1.4]">Find the Property of Your Dreams </h1>
+  </div>
+
+        <div className="w-[500px] flex item-center justify-center mt-8 ">
+          <p className ="text-white w-[400px] pt-10  drop-shadow-[0_1px_1px_rgba(0,0,0,0.8)]0">
+Over 100 construction and real astate projects can be found here.
+The building owner selected us over other properties due to the unique nature of our work
+          </p>
+         
         </div>
+
+        <div className='w-[500px] flex item-center justify-center mt-8'>
+        <Button asChild className='w-[150px] bg-green-800 '>
+          <Link href="/property-search">Find Property</Link>
+        </Button>
+        </div>
+        </div>
+
+       
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+     
     </div>
   );
 }
